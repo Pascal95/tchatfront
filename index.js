4155bf876cb1202377476597b3ba8c90aef45e60
PASCALPierrerichard <89923424+Pascal95@users.noreply.github.com>
2022-06-21 11:03:07 +0200

    evolution

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const path = require('path');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const {v4:uuidv4} = require('uuid');
+const router = require('./router')
 
+app.set('view engine', 'ejs');
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.use(session({
+    secret:uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}));
+
+//route
 app.get('/',(req,res) =>{
     res.sendFile(`${__dirname}/public/index.html`)
 
 })
+
+app.get('/connect',(req,res)=> {
+    res.render('login',{title: "Login"})
+})
+
+app.use('/route', router)
+
+//load static asset
+app.use('/static',express.static(path.join(__dirname,'public')))
+app.use('/assets',express.static(path.join(__dirname,'public/assets')))
+
 
 io.on('connection',(socket) => {
     console.log('un utilisateur s\'est connecté');
